73af5ba9830e4b2b36b91ac05312a9ef9ea381d2
kikexd <enrique06660@gmail.com>
2022-05-15 19:18:41 -0400

    add any function test

---
+++ any_function.py
@@ -0,0 +1,59 @@
+import math
+from matplotlib import pyplot as plt
+from sympy.plotting.textplot import linspace
+from scipy import integrate
+from src.symbolic_regression import symbolic_regression
+from src.utils import evaluate, take_n_samples_regular
+
+
+def f(x, a, b, c):
+    return a * x + b * x**2 + c * x**3
+
+
+def df(x, a, b, c):
+    return a + 2 * b * x + 3 * c * x**2
+
+
+def try_any_function():
+    a = 10
+    b = -175
+    c = 2
+
+    time = 100
+    n = 100000
+
+    x = linspace(0, time, n)
+    y = [f(i, a, b, c) for i in x]
+
+    samples = 201
+
+    xs = take_n_samples_regular(samples, x)
+    ys = take_n_samples_regular(samples, y)
+
+    X_samples = [[xs[i], ys[i]] for i in range(len(xs) - 1)]
+    ode = [[(ys[i + 1] - ys[i]) / (xs[i + 1] - xs[i])] for i in range(len(xs) - 1)]
+
+    best_system = symbolic_regression(
+        X_samples,
+        ode,
+        seed_g=0,
+        FEATURES_NAMES=["x", "y"],
+        MAX_GENERATIONS=100,
+        N_GENERATION_OPTIMIZE=1,
+        POP_SIZE=100,
+        TOURNAMENT_SIZE=1,
+        XOVER_PCT=0.5,
+        MAX_DEPTH=10,
+        REG_STRENGTH=20,
+    )
+
+    integrate_gp = lambda X, x: evaluate(best_system, {"x": x, "y": X[0]})
+
+    X_gp, infodict = integrate.odeint(integrate_gp, 0, x, full_output=True)
+
+    X1_gp = X_gp.T
+
+    plt.plot(x, y, label="samples")
+    plt.plot(x, X1_gp[0], label="symbolic regression")
+    plt.legend()
+    plt.show()
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from SIZR import try_zombie_SIZR
+from any_function import try_any_function
 from lotka_volterra import try_lotka_volterra
 from first_sir import try_sir
 from zombie_militia_workers_moles import try_zombie
 
 
 def main():
-    try_lotka_volterra()
+    # try_lotka_volterra()
     # try_sir()
     # try_zombie()
+    # try_zombie_SIZR()
+    try_any_function()
 
 
 if __name__ == "__main__":
