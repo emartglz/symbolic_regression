92afc294054c22e19928e3caf980727112ca0b67
kikexd <enrique06660@gmail.com>
2022-06-05 17:22:49 -0400

    move models into model folder

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from SIZR import try_zombie_SIZR
-from any_function import try_any_function
-from lotka_volterra import try_lotka_volterra
-from first_sir import try_sir
-from zombie_militia_workers_moles import try_zombie
-from CMIRDZKF import try_CMIRDZKF
+from models.SIZR import try_zombie_SIZR
+from models.any_function import try_any_function
+from models.lotka_volterra import try_lotka_volterra
+from models.first_sir import try_sir
+from models.zombie_militia_workers_moles import try_zombie
+from models.CMIRDZKF import try_CMIRDZKF
 
 
 def main():
