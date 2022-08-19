83caa5f2849e8f66ef7704224aa93e71b8e97d96
kikexd <enrique06660@gmail.com>
2022-08-19 12:04:23 -0400

    fix select random node function

--- src/select_random_node.py
+++ src/select_random_node.py
@@ -1,19 +1,21 @@
 from random import random, randint
 
 
-def select_random_node(selected, parent, depth, MAX_DEPTH):
-    if "children" not in selected:
-        return (parent, depth - 1)
-    # favor nodes near the root
-    # if randint(0, MAX_DEPTH) >= depth:
+def flat_tree(selected, depth, MAX_DEPTH):
+    ret = []
+    if "children" not in selected or depth > MAX_DEPTH:
+        return ret
 
-    child_count = len(selected["children"])
-    # completly random
-    if random() <= 0.5 or child_count == 0:
-        return (selected, depth)
-    return select_random_node(
-        selected["children"][randint(0, child_count - 1)],
-        selected,
-        depth + 1,
-        MAX_DEPTH,
-    )
+    ret.append((selected, depth))
+
+    for i in selected["children"]:
+        ret += flat_tree(i, depth + 1, MAX_DEPTH)
+
+    return ret
+
+
+def select_random_node(selected, depth, MAX_DEPTH):
+    flated = flat_tree(selected, depth, MAX_DEPTH)
+
+    flat_count = len(flated)
+    return flated[randint(0, flat_count - 1)]
