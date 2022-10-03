3ab62d33b92fe44dd26b599ba2a6381c54eb72e9
kikexd <enrique06660@gmail.com>
2022-10-02 21:47:34 -0400

    fixing sir with noise model

--- models/sir_with_noise.py
+++ models/sir_with_noise.py
@@ -1,73 +1,88 @@
 from matplotlib import pyplot as plt
-from sympy.plotting.textplot import linspace
 from scipy import integrate
-from models.utils import add_noise, derivate
+from models.utils import add_noise_and_get_data, integrate_model
 from src.symbolic_regression import symbolic_regression
 from src.utils import evaluate, get_results, save_results, take_n_samples_regular
 
 
 def sir_dx(X, t, a, b):
-    S = X[0]
-    I = X[1]
-    R = X[2]
+    S, I, R = X
 
     return [-a * I * S, a * I * S - b * I, b * I]
 
 
-def integrate_sir(time, samples, X0, a, b):
-    t = linspace(0, time, samples)
-
-    X, infodict = integrate.odeint(sir_dx, X0, t, (a, b), full_output=True)
-
-    S, I, R = X.T
-
-    return (t, S, I, R)
-
-
 def try_sir():
-    a = 0.3
-    b = 0.4
+    a = 0.0003
+    b = 0.1
 
-    X0 = [0.7, 0.3, 0]
+    X0 = [700, 300, 0]
 
     time = 20
     n = 10000
 
     samples = 200
+    symbolic_regression_samples = samples
 
-    noise = 0.01
+    noise = 0.1
+    smoothing_factor = [symbolic_regression_samples * 10000] * 3
 
-    t, S, I, R = integrate_sir(time, n, X0, a, b)
+    t, *X = integrate_model(sir_dx, time, n, X0, a, b)
 
-    ts = take_n_samples_regular(samples, t)
-    Ss = add_noise(take_n_samples_regular(samples, S), noise)
-    Is = add_noise(take_n_samples_regular(samples, I), noise)
-    Rs = add_noise(take_n_samples_regular(samples, R), noise)
+    data = add_noise_and_get_data(
+        t,
+        X,
+        samples,
+        symbolic_regression_samples,
+        noise,
+        smoothing_factor,
+        ["t", "S", "I", "R"],
+        0,
+    )
 
-    X_samples = [
-        {"t": ts[i], "S": Ss[i], "I": Is[i], "R": Rs[i]} for i in range(len(ts))
-    ]
+    X_samples = data["X_samples"]
+    ode = data["ode"]
+    t = data["t"]
+    S, I, R = data["X"]
 
-    convert = [[Ss[i], Is[i], Rs[i]] for i in range(len(ts))]
-    ode = derivate(ts, convert)
+    t_noise = data["t_noise"]
+    S_noise, I_noise, R_noise = data["X_noise"]
+
+    S_spline, I_spline, R_spline = data["X_spline"]
+
+    plt.plot(t_noise, take_n_samples_regular(samples, S), label="S samples")
+    plt.plot(t_noise, take_n_samples_regular(samples, I), label="I samples")
+    plt.plot(t_noise, take_n_samples_regular(samples, R), label="R samples")
+
+    plt.plot(t_noise, S_noise, ".", label="S samples noise")
+    plt.plot(t_noise, I_noise, ".", label="I samples noise")
+    plt.plot(t_noise, R_noise, ".", label="R samples noise")
+
+    ts = list(map(lambda x: x["t"], data["X_samples"]))
+
+    plt.plot(ts, [S_spline(i) for i in ts], "--", label="S samples spline")
+    plt.plot(ts, [I_spline(i) for i in ts], "--", label="I samples spline")
+    plt.plot(ts, [R_spline(i) for i in ts], "--", label="R samples spline")
+
+    plt.legend()
+    plt.show()
 
     results = symbolic_regression(
-        X_samples[:-1],
+        X_samples,
         ode,
         seed_g=0,
         MAX_GENERATIONS=100,
         POP_SIZE=100,
         FEATURES_NAMES=[["S", "I"], ["S", "I"], ["I"]],
-        MUTATION_SIZE=100,
-        XOVER_SIZE=100,
+        MUTATION_SIZE=50,
+        XOVER_SIZE=50,
         MAX_DEPTH=5,
         REG_STRENGTH=20,
         verbose=True,
     )
 
-    # best_system = get_results("SIR")
+    # results = get_results("SIR_noise")
     best_system = results["system"]
-    save_results(results, "SIR")
+    save_results(results, "SIR_noise")
 
     integrate_gp = lambda X, t: evaluate(
         best_system, {"t": t, "S": X[0], "I": X[1], "R": X[2]}
@@ -77,15 +92,23 @@ def try_sir():
 
     S_gp, I_gp, R_gp = SIR_gp.T
 
-    plt.plot(ts, take_n_samples_regular(samples, S), label="S samples")
-    plt.plot(ts, take_n_samples_regular(samples, I), label="I samples")
-    plt.plot(ts, take_n_samples_regular(samples, R), label="R samples")
-    plt.plot(ts, Ss, label="S samples noise")
-    plt.plot(ts, Is, label="I samples noise")
-    plt.plot(ts, Rs, label="R samples noise")
-    plt.plot(t, S_gp, label="S symbolic regression")
-    plt.plot(t, I_gp, label="I symbolic regression")
-    plt.plot(t, R_gp, label="R symbolic regression")
+    plt.plot(t_noise, take_n_samples_regular(samples, S), label="S samples")
+    plt.plot(t_noise, take_n_samples_regular(samples, I), label="I samples")
+    plt.plot(t_noise, take_n_samples_regular(samples, R), label="R samples")
+
+    plt.plot(t_noise, S_noise, ".", label="S samples noise")
+    plt.plot(t_noise, I_noise, ".", label="I samples noise")
+    plt.plot(t_noise, R_noise, ".", label="R samples noise")
+
+    ts = list(map(lambda x: x["t"], data["X_samples"]))
+
+    plt.plot(ts, [S_spline(i) for i in ts], "--", label="S samples spline")
+    plt.plot(ts, [I_spline(i) for i in ts], "--", label="I samples spline")
+    plt.plot(ts, [R_spline(i) for i in ts], "--", label="R samples spline")
+
+    plt.plot(t, S_gp, "-.", label="S symbolic regression")
+    plt.plot(t, I_gp, "-.", label="I symbolic regression")
+    plt.plot(t, R_gp, "-.", label="R symbolic regression")
     plt.legend()
     plt.show()
 
