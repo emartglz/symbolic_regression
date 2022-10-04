7706f9194fce3dd683013b6808c5067e9bb56740
kikexd <enrique06660@gmail.com>
2022-10-04 19:48:56 -0400

    factorize lotka volterra model

--- models/lotka_volterra.py
+++ models/lotka_volterra.py
@@ -1,8 +1,13 @@
 from matplotlib import pyplot as plt
 from scipy import integrate
-from models.utils import add_noise_and_get_data, integrate_model
+from models.utils import (
+    add_noise_and_get_data,
+    integrate_model,
+    plot_data,
+    separate_samples,
+)
 from src.symbolic_regression import symbolic_regression
-from src.utils import evaluate, get_results, save_results, take_n_samples_regular
+from src.utils import evaluate, get_results, save_results
 
 
 def lotka_volterra_dx(X, t, a, b, c, d):
@@ -30,6 +35,8 @@ def try_lotka_volterra():
         symbolic_regression_samples * 1000,
     ]
 
+    variable_names = ["t", "X1", "X2"]
+
     t, *X = integrate_model(lotka_volterra_dx, time, n, X0, a, b, c, d)
 
     data = add_noise_and_get_data(
@@ -39,33 +46,23 @@ def try_lotka_volterra():
         symbolic_regression_samples,
         noise,
         smoothing_factor,
-        ["t", "X1", "X2"],
+        variable_names,
         0,
     )
-
     X_samples = data["X_samples"]
     ode = data["ode"]
-    t = data["t"]
-    X1, X2 = data["X"]
 
-    t_noise = data["t_noise"]
-    X1_noise, X2_noise = data["X_noise"]
+    t_spline, *X_spline = separate_samples(variable_names, X_samples)
 
-    X1_spline, X2_spline = data["X_spline"]
-
-    plt.plot(t_noise, take_n_samples_regular(samples, X1), label="X1 samples")
-    plt.plot(t_noise, take_n_samples_regular(samples, X2), label="X2 samples")
-
-    plt.plot(t_noise, X1_noise, ".", label="X1 samples noise")
-    plt.plot(t_noise, X2_noise, ".", label="X2 samples noise")
-
-    ts = list(map(lambda x: x["t"], data["X_samples"]))
-
-    plt.plot(ts, [X1_spline(i) for i in ts], "--", label="X1 samples spline")
-    plt.plot(ts, [X2_spline(i) for i in ts], "--", label="X2 samples spline")
-
-    plt.legend()
-    plt.show()
+    plot_data(
+        variables_names=variable_names[1:],
+        t_samples=data["t"],
+        samples=data["X"],
+        t_noise=data["t_noise"],
+        samples_noise=data["X_noise"],
+        t_spline=t_spline,
+        samples_spline=X_spline,
+    )
 
     results = symbolic_regression(
         X_samples,
@@ -80,30 +77,25 @@ def try_lotka_volterra():
         verbose=True,
     )
 
-    # results = get_results("LV2")
+    # results = get_results("LV")
     best_system = results["system"]
-    save_results(results, "LV2")
+    save_results(results, "LV")
 
     integrate_gp = lambda X, t: evaluate(best_system, {"t": t, "X1": X[0], "X2": X[1]})
     X_gp, infodict = integrate.odeint(integrate_gp, X0, t, full_output=True)
-    X1_gp, X2_gp = X_gp.T
+    X_gp = X_gp.T.tolist()
 
-    plt.plot(t_noise, take_n_samples_regular(samples, X1), label="X1 samples")
-    plt.plot(t_noise, take_n_samples_regular(samples, X2), label="X2 samples")
-
-    plt.plot(t_noise, X1_noise, ".", label="X1 samples noise")
-    plt.plot(t_noise, X2_noise, ".", label="X2 samples noise")
-
-    ts = list(map(lambda x: x["t"], data["X_samples"]))
-
-    plt.plot(ts, [X1_spline(i) for i in ts], "--", label="X1 samples spline")
-    plt.plot(ts, [X2_spline(i) for i in ts], "--", label="X2 samples spline")
-
-    plt.plot(t, X1_gp, "-.", label="X1 symbolic regression")
-    plt.plot(t, X2_gp, "-.", label="X2 symbolic regression")
-
-    plt.legend()
-    plt.show()
+    plot_data(
+        variables_names=variable_names[1:],
+        t_samples=data["t"],
+        samples=data["X"],
+        t_noise=data["t_noise"],
+        samples_noise=data["X_noise"],
+        t_spline=t_spline,
+        samples_spline=X_spline,
+        t_symbolic_regression=t,
+        samples_symbolic_regression=X_gp,
+    )
 
 
 if __name__ == "__main__":
